440d6a36cfe2b3be55481d77f1fb96a3d6435edb
ts <ts.91358922@gmail.com>
2024-07-29 18:58:48 +0900

    検索機能の実装

--- routes/search.js
+++ routes/search.js
@@ -4,27 +4,27 @@ const MAX_ITEMS = 5;
 
 router.get("/", async (req, res, next) => {
   let keyword = req.query.keyword || "";
-  console.log(`Searching for keyword: ${keyword}`); // キーワードをログに出力
   let results;
 
   try {
     if (keyword) {
       const query = await sql("SELECT_SHOP_LIST_BY_NAME");
-      console.log(`SQL Query for searching: ${query}`); // SQLクエリをログに出力
       results = await MySQLClient.executeQuery(
         query,
         [`%${keyword}%`, MAX_ITEMS]
       );
     } else {
       const query = await sql("SELECT_SHOP_HIGH_SCORE_LIST");
-      console.log(`SQL Query for high score list: ${query}`); // SQLクエリをログに出力
       results = await MySQLClient.executeQuery(
         query,
         [MAX_ITEMS]
       );
     }
 
-    res.render("./search/list.ejs", { results });
+    res.render("./search/list.ejs", {
+      keyword,
+      results
+    });
   } catch (err) {
     console.error(err); // エラーをログに出力
     next(err);
